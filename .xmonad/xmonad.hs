144a497cbec2aed445a676d2ca91e28b9de9503b
Tim Dysinger <tim@dysinger.net>
2012-03-02 06:38:47 -1000

    Updated xmonad config with EZConfig

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,40 +4,47 @@ import XMonad.Actions.WithAll
 import XMonad.Config.Xfce
 import XMonad.Hooks.ManageHelpers
 import XMonad.Prompt
+import XMonad.Prompt.AppendFile
 import XMonad.Prompt.Shell
-import XMonad.Util.CustomKeys
+import XMonad.Util.EZConfig
+
+myConfig =
+  xfceConfig { modMask    = mod4Mask
+             , manageHook = manageHook xfceConfig <+> myManageHook
+             } `additionalKeysP` myKeys
+
+myManageHook =
+  composeAll [ className =? "Xfce4-notifyd" --> doIgnore
+             , doCenterFloat
+             ]
+
+myKeys =
+  [ -- workspaces
+    ("M-<Up>", prevWS)
+  , ("M-<Left>", prevWS)
+  , ("M-<Right>", nextWS)
+  , ("M-<Down>", nextWS)
+  , ("M-z", toggleWS)
+    -- window management
+  , ("M-S-<Up>", shiftToPrev)
+  , ("M-S-<Left>", shiftToPrev)
+  , ("M-S-<Right>", shiftToNext)
+  , ("M-S-<Down>", shiftToNext)
+  , ("M-s", sinkAll)
+    -- quick-launch
+  , ("M-f", spawn "firefox")
+  , ("M-<Print>", spawn "scrot ~/screenshot-%Y-%m-%d.png")
+  , ("M-<Home>", spawn "thunar ~")
+    -- quick-prompts
+  , ("M-n", appendFilePrompt "~/org/inbox.org")
+  , ("M-C-<Space>", shellPrompt myXPConfig)
+  ]
+
+myXPConfig =
+  greenXPConfig { autoComplete = Just 1
+                , font         = myFont
+                , position     = Bottom
+                }
+myFont = "xft:inconsolata:bold:size=13:antialias=true:hinting=light"
 
 main = xmonad myConfig
-  where
-    myConfig     = xfceConfig { modMask    = myMask
-                              , keys       = myKeys
-                              , manageHook = myManageHook
-                              }
-    myXPConfig   = greenXPConfig { autoComplete = Just 1
-                                 , font         = myFont
-                                 , position     = Bottom
-                                 }
-    myManageHook = manageHook xfceConfig <+> composeAll
-                   [ className =? "Xfce4-notifyd" --> doIgnore
-                   , doCenterFloat
-                   ]
-    myKeys       = customKeys (\_ -> []) (\_ -> myKeysToAdd)
-    myRunMask    = myMask .|. controlMask
-    myMask       = mod4Mask
-    myFont       = "xft:inconsolata:bold:size=13:antialias=true:hinting=light"
-    myKeysToAdd  =
-      [ ((myMask,               xK_Right), nextWS)
-      , ((myMask,               xK_Down),  nextWS)
-      , ((myMask,               xK_Left),  prevWS)
-      , ((myMask,               xK_Up),    prevWS)
-      , ((myMask .|. shiftMask, xK_Right), shiftToNext)
-      , ((myMask .|. shiftMask, xK_Down),  shiftToNext)
-      , ((myMask .|. shiftMask, xK_Left),  shiftToPrev)
-      , ((myMask .|. shiftMask, xK_Up),    shiftToPrev)
-      , ((myMask, xK_a), sinkAll)
-      , ((myMask, xK_z), toggleWS)
-      , ((myMask, xK_Print), spawn "scrot ~/screenshot-%Y-%m-%d.png")
-      , ((myRunMask, xK_space), shellPrompt myXPConfig)
-      , ((myRunMask, xK_f), spawn "firefox")
-      , ((myRunMask, xK_h), spawn "thunar ~")
-      ]
