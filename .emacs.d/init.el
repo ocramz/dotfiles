7407b73cb5a82391bdf6714878415e7693b8186a
Tim Dysinger <tim@dysinger.net>
2012-03-02 13:15:53 -1000

    Move my custom.el settings over to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -71,6 +71,52 @@
         ("vr" "View @READ"     tags "+TODO=\"TODO\"+\@READ"     nil)
         ("vs" "View @SHOP"     tags "+TODO=\"TODO\"+\@SHOP"     nil)
         ("vw" "View @WORK"     tags "+TODO=\"TODO\"+\@WORK"     nil))
+      org-agenda-dim-blocked-tasks 'invisible
+      org-agenda-skip-deadline-if-done t
+      org-agenda-skip-deadline-prewarning-if-scheduled t
+      org-agenda-skip-scheduled-if-done t
+      org-agenda-todo-ignore-deadlines 'far
+      org-agenda-todo-ignore-scheduled 'all
+      org-archive-location ".%s::"
+      org-capture-templates '(("t" "Todo" entry
+                               (file "~/org/inbox.org")
+                               "* TODO %?" CAPTURED %u))
+      org-clock-idle-time 15
+      org-completion-use-ido t
+      org-enable-priority-commands nil
+      org-enforce-todo-checkbox-dependencies t
+      org-enforce-todo-dependencies t
+      org-export-latex-tables-centered t
+      org-hide-leading-stars t
+      org-mobile-inbox-for-pull "~/org/from-mobile.org"
+      org-mobile-use-encryption t
+      org-log-done 'time
+      org-log-into-drawer t
+      org-log-repeat 'time
+      org-modules '(org-bbdb
+                    org-bibtex
+                    org-crypt
+                    org-docview
+                    org-gnus
+                    org-info
+                    org-jsinfo
+                    org-habit
+                    org-irc
+                    org-mew
+                    org-mhe
+                    org-rmail
+                    org-vm
+                    org-wl
+                    org-w3m)
+      org-odd-levels-only t
+      org-outline-path-complete-in-steps nil
+      org-refile-allow-creating-parent-nodes 'confirm
+      org-refile-use-outline-path 'file
+      org-refile-targets '((nil :maxlevel . 3)
+                           (org-agenda-files :maxlevel . 1))
+      org-return-follows-link t
+      org-src-tab-acts-natively t
+      org-src-fontify-natively t
       org-stuck-projects '("+TODO=\"PROJ\"" ("TODO") nil "")
       org-tag-alist '(("@COMPUTER" . ?c)
                       ("@ERRAND"   . ?e)
@@ -83,7 +129,9 @@
                                     "|"
                                     "DONE(d!)")
                           (sequence "|"
-                                    "SKIP(s@)")))
+                                    "SKIP(s@)"))
+      org-todo-repeat-to-state "TODO"
+      org-use-sub-superscripts '{})
 
 (define-key global-map (kbd "C-c a") 'org-agenda)
 (define-key global-map (kbd "C-c c") 'org-capture)
