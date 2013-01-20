162b43f624cc18293cd3933ae4a5fc42d284077b
Tim Dysinger <tim@dysinger.net>
2013-01-19 17:29:18 -1000

    switch Agda from el-get to 'local disk load'

--- recipes/agda-mode.el
+++
@@ -1,5 +0,0 @@
-(:name agda-mode
-       :type http
-       :url "http://code.haskell.org/Agda/src/data/emacs-mode/agda2-mode.el"
-       :description "Emacs mode for the Agda Language"
-       :features agda-mode)
