cdbaa1f5c67fe4761412015fd444b2a78f6191f5
Tim Dysinger <tim@dysinger.net>
2015-02-01 09:19:54 -1000

    add smartparens-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,6 +52,7 @@
         (rainbow-delimiters  . "melpa")
         (rainbow-identifiers . "melpa")
         (smex                . "melpa")
+        (smartparens-mode    . "melpa")
         (textile-mode        . "melpa")
         (yaml-mode           . "melpa")
         (yasnippet           . "melpa"))
@@ -271,6 +272,7 @@
 (add-hook 'haskell-mode-hook 'ghc-init)
 (add-hook 'haskell-mode-hook 'hindent-mode)
 (add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
+(add-hook 'haskell-mode-hook 'smartparens-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
