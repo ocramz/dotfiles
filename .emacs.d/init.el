5ffffd48fb76f3bfb4c7aa450008c70686def9fa
Tim Dysinger <tim@dysinger.net>
2015-05-05 18:44:45 -1000

    update haskell mode bindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -265,18 +265,24 @@
 (autoload 'ghc-debug "ghc" nil t)
 
 (defun my-haskell-mode-hook ()
-  (local-set-key (kbd "C-c C-b") 'haskell-interactive-switch)
+  (local-set-key (kbd "C-c C-b") 'haskell-interactive-bring)
+  (local-set-key (kbd "C-c C-c") 'haskell-process-cabal-build)
   (local-set-key (kbd "C-c C-i") 'haskell-process-do-info)
-  (local-set-key (kbd "C-c C-l") 'haskell-process-load-file)
+  (local-set-key (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (local-set-key (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (local-set-key (kbd "C-c C-s") 'haskell-interactive-switch)
   (local-set-key (kbd "C-c C-t") 'haskell-process-do-type)
-  (local-set-key (kbd "C-c C-z") 'haskell-interactive-switch)
-  (local-set-key (kbd "C-c v c") 'haskell-cabal-visit-file))
+  (local-set-key (kbd "C-c c")   'haskell-process-cabal)
+  (local-set-key (kbd "C-c v c") 'haskell-cabal-visit-file)
+  (local-set-key (kbd "SPC")     'haskell-mode-contextual-space))
 
 (add-hook 'haskell-mode-hook 'ghc-init)
 (add-hook 'haskell-mode-hook 'hindent-mode)
-(add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+(add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
 
 (with-eval-after-load 'company
   (add-to-list 'company-backends 'company-ghc)
